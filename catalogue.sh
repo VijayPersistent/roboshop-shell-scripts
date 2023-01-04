e0fb423a0fb541e6cff7f4cdecb1964396b9dee5
vijayabhaskar <vijayabhaskarmakke@gmail.com>
2023-01-04 17:26:39 +0530

    if condition

--- catalogue.sh
+++ catalogue.sh
@@ -1,40 +1,72 @@
 LOG_FILE=/tmp/catalogue
 echo "setup nodejs repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = SUCCESS
+else
+  echo Status = FAILURE
+fi
 
 echo "install nodejs"
 yum install nodejs -y &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = SUCCESS
+else
+  echo Status = FAILURE
+fi
 
 echo "add application roboshop user"
 useradd roboshop &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = SUCCESS
+else
+  echo Status = FAILURE
+fi
 
 echo "download catalogue application code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = SUCCESS
+else
+  echo Status = FAILURE
+fi
 
 cd /home/roboshop
 
 echo "extract catalogue application code"
 unzip /tmp/catalogue.zip &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = SUCCESS
+else
+  echo Status = FAILURE
+fi
 
 mv catalogue-main catalogue
 cd /home/roboshop/catalogue
 
 echo "install nodejs dependencies"
 npm install &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = SUCCESS
+else
+  echo Status = FAILURE
+fi
 
 echo "setup catalogue service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = SUCCESS
+else
+  echo Status = FAILURE
+fi
 
 systemctl daemon-reload &>>${LOG_FILE}
 systemctl enable catalogue &>>${LOG_FILE}
 
 echo "start catalogue service"
 systemctl start catalogue &>>${LOG_FILE}
-echo status = $?
+if [ $? -eq 0 ]; then
+  echo Status = SUCCESS
+else
+  echo Status = FAILURE
+fi
